24c45c6b9be821c57bb934b594832da0494860f7
guilhermedlacerda <guilhermedossantoslacerda@gmail.com>
2025-04-08 08:20:14 -0300

    Organizando

--- database/migrations/2025_04_01_132607_create_administradors_table.php
+++ database/migrations/2025_04_01_132607_create_administradors_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('nome');
             $table->string('cpf')->unique();
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('senha');
             $table->timestamps();
         });
     }
